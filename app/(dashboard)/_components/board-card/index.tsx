238df20c2d56a3ec0a8d32ab571ae1abd4fad5d1
sun <743685264@qq.com>
2024-03-03 22:52:23 +0800

    feat: board rename & delete

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { useAuth } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 
+import { Actions } from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { Overlay } from "./overlay";
@@ -49,6 +51,19 @@ export const BoardCard = ({
                         className="object-fit"
                     />
                     <Overlay />
+                    <Actions
+                        id={id}
+                        title={title}
+                        side="right"
+                    >
+                        <button
+                            className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+                            <MoreHorizontal
+                                className="text-white opacity-75
+                                hover:opacity-100 transition-opacity"
+                            />
+                        </button>
+                    </Actions>
                 </div>
                 <Footer 
                     isFavorite={isFavorite}
